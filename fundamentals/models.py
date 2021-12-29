6cb5e01cc91e32efe2a3c22a9a078c8632f79492
shamimferdous <shamimferdous5@gmail.com>
2021-12-29 08:24:11 +0600

    code commit

--- fundamentals/models.py
+++ fundamentals/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+# Zone Model
+class Zone(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    district = models.CharField(max_length=50, null=False, blank=False)
+    sub_district = models.CharField(max_length=50, null=False, blank=False)
+    division = models.CharField(max_length=50, null=False, blank=False)
+
+    class Meta:
+        db_table = 'zones'
